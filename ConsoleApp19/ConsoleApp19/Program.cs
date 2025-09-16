b7f39442b4afa534224d95643d22feb3347faf2a
Gzhegozh Bzhenchishchikevich <moskalyovilya116@gmail.com>
2025-09-16 20:56:36 +0300

    Use proper grammar.

--- ConsoleApp19/ConsoleApp19/Program.cs
+++ ConsoleApp19/ConsoleApp19/Program.cs
@@ -10,9 +10,11 @@ namespace ConsoleApp19
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What is your name?");
-            string s = Console.ReadLine();
-            Console.WriteLine("Ahmat is power! " + s + ".");
+            Console.WriteLine("What is your first name?");
+            string s2, s1 = Console.ReadLine();
+            Console.WriteLine("What is your last name?");
+            s2 = Console.ReadLine();
+            Console.WriteLine("Ahmat is power, " + s1 + " " + s2 + "!");
         }
     }
 }
