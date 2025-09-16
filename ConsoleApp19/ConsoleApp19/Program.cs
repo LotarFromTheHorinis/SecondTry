c514927e707dab0e87c3c75b5917351ddb048607
Gzhegozh Bzhenchishchikevich <moskalyovilya116@gmail.com>
2025-09-17 00:14:36 +0300

    Mental break-out

--- ConsoleApp19/ConsoleApp19/Program.cs
+++ ConsoleApp19/ConsoleApp19/Program.cs
@@ -10,11 +10,8 @@ namespace ConsoleApp19
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What is your first name?");
-            string s2, s1 = Console.ReadLine();
-            Console.WriteLine("What is your last name?");
-            s2 = Console.ReadLine();
-            Console.WriteLine("Ahmat is power, " + s1 + " " + s2 + "!");
+            Console.WriteLine("killmeplease");
+            
         }
     }
 }
